64c406a0f4ae9c83e0a183d891f5d126e6a00af1
Attila Bordan <bordan_attila@hotmail.com>
2022-07-29 05:51:21 -0400

    Add scores and name to local storage

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var questionArea = document.querySelector("h1");
 var firstOption = document.querySelector(".answer1");
 var secondOption = document.querySelector(".answer2");
 var thirdOption = document.querySelector(".answer3");
-var fourthOption = document.querySelector("answer4");
+var fourthOption = document.querySelector(".answer4");
 var startButton = document.querySelector(".btn");
 var theArea = document.querySelector("section");
 var answerCheck = document.querySelector(".check");
@@ -11,10 +11,15 @@ var selected = "";
 var questionCount = 1;
 var theTimer = document.querySelector(".timer");
 var timerCount;
-var correctCount;
+var correctCount = 0;
+var entryField = document.querySelector(".initials");
+var playerName = document.querySelector("#player");
+var pname = "";
+var highestScore = 0;
+var higestName = "";
 
 // Create  question database object
-// var questionData = require("./questions.json")
+
 var questionData = 
 {
     "1":
@@ -32,7 +37,7 @@ var questionData =
     "optionA": "Central Process Unit",
     "optionB": "Central Processor Unit",
     "optionC": "Central Processing Unit",
-    "optionD": "What",
+    "optionD": "Computer Personal Unit",
     "correct": "Central Processing Unit",
     },
     "3": 
@@ -53,7 +58,17 @@ var questionData =
     "optionD": "Unlimited",
     "correct": "1GB",
     },
+    "5": 
+    {
+    "question": "What does the Prt Sc button do?",
+    "optionA": "Nothing",
+    "optionB": "Closes all windows",
+    "optionC": "Saves a .png file of what's on the screen in your screenshots folder in photos",
+    "optionD": "Captures what's on the screen and copies it to your clipboard",
+    "correct": "Captures what's on the screen and copies it to your clipboard",
+    },
 }
+var size = Object.keys(questionData).length
 
 // Add eventListener to the start button. Hide button once it is clicked
 document.querySelector(".btn").addEventListener("click", function () {
@@ -63,13 +78,22 @@ document.querySelector(".btn").addEventListener("click", function () {
     startQuiz(questionData);
 })
 
+//Add eventListener to View High Score
+document.querySelector(".viewHigh").addEventListener("click", function () {
+    showHigh();
+})
+
 // Create the answer options
 startQuiz = function (questionData) {
+    if (questionCount > size) {
+        gameOver();
+    } else {
     questionArea.textContent = questionData[questionCount].question;
     firstOption.textContent = questionData[questionCount].optionA;
     secondOption.textContent = questionData[questionCount].optionB;
     thirdOption.textContent = questionData[questionCount].optionC;
-    // fourthOption.textContent = questionData[questionCount].optionD;
+    fourthOption.textContent = questionData[questionCount].optionD;
+    }
 }
 
 // Add eventListener to the answer options
@@ -94,13 +118,46 @@ checkAnswer = function (selected) {
 
 // Create timer function
 startTimer = function () {
-    timerCount = 30;
+    timerCount = 10;
     var timer = setInterval(function () {
         timerCount--;
         theTimer.textContent = "Timer: " + timerCount;
         if (timerCount === 0) {
             questionArea.textContent = "Game Over";
             clearInterval(timer);
+            gameOver();
         }
     }, 1000);
+}
+
+// Create game over screen
+gameOver = function () {
+    questionArea.textContent = "End of Game";
+    firstOption.textContent = "Your final score is " + correctCount + ".";
+    secondOption.textContent = "";
+    thirdOption.textContent = "";
+    fourthOption.textContent = "";
+    entryField.classList.remove("initials");
+    store()
+}
+
+// Store data in local storage
+store = function () {
+    document.querySelector(".submit").addEventListener("click", function () {
+        pname = playerName.value;
+        console.log(pname)
+        localStorage.setItem("playername", pname);
+        localStorage.setItem("score", correctCount)
+        var currentHighScore = localStorage.getItem("Higest score")
+        console.log(currentHighScore)
+        if (correctCount > currentHighScore) {
+            localStorage.setItem("Higest score", correctCount);
+            localStorage.setItem("Higest Score player", pname);
+        }
+    })
+}  
+
+showHigh = function () {
+    questionArea.textContent = "High Score";
+    
 }
